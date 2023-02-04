9c193e243e2e33f4b75938312ef728c4b515dac5
Asad Mehmood12 <masad3421@yahoo.com>
2023-02-04 16:38:08 +0500

    layout is ready

--- dashbord/src/App.js
+++ dashbord/src/App.js
@@ -1,12 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Layout from './components/Layout';
 
 function App() {
   return (
-    <div className="App">
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="App bg-slate-800 h-screen">
+      <Layout />
     </div>
   );
 }
---
+++ dashbord/src/components/Layout.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Layout() {
+    return ( 
+    <div className="border flex items-center justify-center h-screen">
+        <div className="border max-w-md mx-auto">
+            <h1>ssjsjsjs</h1>
+         
+        </div>
+    </div> 
+    );
+}
+
+export default Layout;
